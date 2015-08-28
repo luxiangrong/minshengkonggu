c76a2d84cfaf840f4252036e701f4df51ed3bed4
LuXiangrong <luxiangrongwave@gmail.com>
2015-08-28 23:23:45 +0800

    全屏切初步完成

---
+++ app/scripts/main.js
@@ -0,0 +1,7 @@
+(function(jQuery){
+	$(document).ready(function(){
+		$('#fullpage').fullpage({
+			anchors: ['banner', 'industry', 'culture', 'news', 'caring-society', 'footer']
+		});
+	});
+})($);
