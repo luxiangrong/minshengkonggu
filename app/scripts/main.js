cd3dd5b61c32a30b136f23341b21d8947df1c159
LuXiangrong <luxiangrongwave@gmail.com>
2015-09-09 08:07:22 +0800

    一些修改

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,5 @@
 (function(jQuery){
 	$(document).ready(function(){
-		$('#fullpage').fullpage({
-			anchors: ['banner', 'industry', 'culture', 'news', 'caring-society', 'footer']
-		});
+		
 	});
 })($);
